4a974bfaacc5a8b7c8da5ecd8ad67829a3fa84bf
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-03 11:34:51 -0400

    👌 IMPROVE: Refactor
    - clean up comments

--- 03 - CSS Variables/main.js
+++ 03 - CSS Variables/main.js
@@ -3,6 +3,28 @@ const inputs = document.querySelectorAll('.controls input');
 
 // main function
 function handleUpdate(){
+  // grab dataset of elements with a suffix "px", OR Nothing (to handle undefined)
+  const suffix = this.dataset.sizing || ''; // get a suffix to concat
+
+  // access root, style, set property of current variable to current value, append suffix 'px' if needed
+  document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+// interate over each input and place smurfs, call main function
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+
+
+
+
+/* OLD CODE
+// select all inputs on the page
+const inputs = document.querySelectorAll('.controls input');
+
+// main function
+function handleUpdate(){
   // console.log(this.value);
   // grab dataset of elements with a suffix "px", OR Nothing (to handle undefined)
   const suffix = this.dataset.sizing || ''; // get a suffix to concat
@@ -17,3 +39,4 @@ function handleUpdate(){
 // interate over each input and place smurfs, call main function
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+*/
