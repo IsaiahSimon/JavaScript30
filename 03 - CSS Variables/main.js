43fc4a22eac3d8fc55edb9ac69697c17a958c05a
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-03 11:32:54 -0400

    📦 NEW: Add basic functionality

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,19 @@
+// select all inputs on the page
+const inputs = document.querySelectorAll('.controls input');
+
+// main function
+function handleUpdate(){
+  // console.log(this.value);
+  // grab dataset of elements with a suffix "px", OR Nothing (to handle undefined)
+  const suffix = this.dataset.sizing || ''; // get a suffix to concat
+  // console.log(suffix);
+
+  // console.log(this.name); //access the HTML 'name' attribute of each input
+
+  // access root, style, set property of current variable to current value, append suffix 'px' if needed
+  document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+// interate over each input and place smurfs, call main function
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
